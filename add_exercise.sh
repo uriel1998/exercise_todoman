681cf0ee2cc2c36521d931ae605f19086e70493c
Steven Saus <steven@stevesaus.com>
2025-02-15 19:32:49 -0500

    Sketched in exercise adding script

--- add_exercise.sh
+++ add_exercise.sh
@@ -1,13 +1,122 @@
 #!/bin/bash
 
+# grep sed awk detox
+
+##############################################################################
+#  fit_todoman
+#  Some simple exercise tracking / adding scripts using todoman
+#  (c) Steven Saus 2025
+#  Licensed under the MIT license
+#
+##############################################################################
+
+##############################################################################
+# Variables
+##############################################################################
+
+VERSION="0.1.0"
+export SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+LOUD=0
+DAYOFWEEK=$(date +%A)
+DATE=$(date +%Y-%m-%d)
+LIST=""
+INIFILE=""
+
+function loud() {
+##############################################################################
+# loud outputs on stderr 
+##############################################################################    
+    if [ $LOUD -eq 1 ];then
+        echo "$@" 1>&2
+    fi
+}
+
+display_help(){
+##############################################################################
+# Show the Help
+##############################################################################    
+    echo "###################################################################"
+    echo "# Standalone: /path/to/FILENAME.sh [options]"
+    echo "# Info ############################################################"
+    echo "# --help:  show help "
+    echo "# --readme: display the README on the console"
+    echo "# Usage ###########################################################"    
+    echo "###################################################################"
+}
 
 
+###############################################################################
+# Establishing XDG directories, or creating them if needed.
+# Find config ini file
+############################################################################### 
 
-# Find config
-# get list to use from top
-# what day of week is it
-# find day of week heading
-# each line is an exercise formatted
-# exercise description and stuff;HH:MM
-# if HHMM is empty, substitute 00:00
-# run it at midnight, add each line as a task
+
+if [ -z "${XDG_DATA_HOME}" ];then
+    export XDG_DATA_HOME="${HOME}/.local/share"
+    export XDG_CONFIG_HOME="${HOME}/.config"
+    export XDG_CACHE_HOME="${HOME}/.cache"
+fi
+
+if [ ! -d "${XDG_CONFIG_HOME}" ];then
+    loud "Your XDG_CONFIG_HOME variable is not properly set and does not exist."
+    exit 99
+fi
+
+if [ -f "${XDG_CONFIG_HOME}/fit_todoman.ini" ];then
+    INIFILE="${XDG_CONFIG_HOME}/fit_todoman.ini"
+else 
+    if [ -f "${SCRIPT_DIR}/fit_todoman.ini" ];then
+        INIFILE="${SCRIPT_DIR}/fit_todoman.ini"
+    else
+        loud "Config file does not exist in ${XDG_CONFIG_HOME} or ${SCRIPT_DIR}!"
+        exit 99
+    fi
+fi
+
+# get list to use from INI
+LIST=$(grep "LIST_TO_USE" "${INIFILE}")
+
+
+found=0
+while IFS= read -r line; do
+    if [[ $found -eq 0 ]]; then
+        # find day of week heading
+        # Check if the line contains DAYOFWEEK
+        if echo "$line" | grep -q "$DAYOFWEEK"; then
+            found=1
+            loud "Found $DAYOFWEEK in $line"
+            loud "Now to read in exercises for $DAYOFWEEK"
+        fi
+    else
+        # If a line starts with [ then break out of the loop.
+        if [[ $line =~ ^\[ ]]; then
+            break
+        fi
+        # It's an exercise line.
+        # each line is an exercise formatted
+        # exercise description and stuff;HH:MM
+        # Note separator is a semicolon
+        # if HHMM is empty, substitute 00:00
+        # run it at midnight, add each line as a task
+        exercise_description=$(echo "${line}" | awk '{ print $1 }' | sed 's|["]|“|g' | sed 's|['\'']|’|g' | detox --inline)
+            #extra sed to unsmarten stray quotes, also detox            
+        exercise_time=$(echo "${line}" | awk '{ print $2 }')
+        if [[ $exercise_time =~ ^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$ ]]; then
+            loud "Valid time format"
+        else
+            loud "Invalid time format, using 20:00 (8pm)"
+            exercise_time="20:00"
+        fi
+        if [ "$LIST" != "" ];then
+            todo new \""${exercise_description}"\" -d $DATE $exercise_time --list \"${LIST}\"
+        else
+            #no special list used
+            todo new \""${exercise_description}"\" -d $DATE $exercise_time 
+        fi
+        
+    fi
+done < "${INIFILE}"
+
+
+ 
+
